9a9cc16d80c793f5f0b3ec09c73a64cfde61d720
Ritik Pandey <128275361+ritik-pandey527@users.noreply.github.com>
2025-03-07 14:27:36 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 @app.route('/fall_data', methods=['POST'])
 def fall_data():
